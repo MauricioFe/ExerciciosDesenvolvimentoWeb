94992789cd72be1f81f613ee9ea40c7b61a89af6
Maurício Fonseca <mauricio.fonseca@agiltec.com>
2021-05-17 20:11:20 -0300

    Finalizando projeto do jokenpo

--- TrabalhoEquipeJS/Jokenpo/js/script.js
+++ TrabalhoEquipeJS/Jokenpo/js/script.js
@@ -1,5 +1,5 @@
-let pontuacaoJogador,
-    pontuacaoComputador,
+let pontuacaoJogador = 0,
+    pontuacaoComputador = 0,
     vencedor,
     escolhaJogador,
     escolhaComputador;
@@ -8,8 +8,14 @@ let pontuacaoJogador,
 let pedra = document.querySelector(".pedra");
 let papel = document.querySelector(".papel");
 let tesoura = document.querySelector(".tesoura");
-let pc = document.querySelector(".resultado-pc");
 
+let pedraPc = document.querySelector(".pedra-pc");
+let papelPc = document.querySelector(".papel-pc");
+let tesouraPc = document.querySelector(".tesoura-pc");
+let empate = document.querySelector(".empate");
+
+let iptPontucaoJogador = document.querySelector("#pontuacaoJogador");
+let iptPontucaoComputador = document.querySelector("#pontuacaoComputador");
 function getEscolhaComputador() {
     let escolha = Math.round(Math.random() * 2);
     console.log(escolha);
@@ -23,7 +29,6 @@ function getEscolhaComputador() {
         case 2:
             escolhaComputador = "tesoura"
             break;
-
     }
     return escolhaComputador;
 }
@@ -33,39 +38,79 @@ function getEscolhaComputador() {
 pedra.addEventListener('click', () => {
     escolhaJogador = "pedra";
     let escolhaComputador = getEscolhaComputador();
-    pc.innerHTML = `<h2>A escolha do computador foi:</h2>
-    <img src='img/${escolhaComputador}.png'/>`;
-    console.log(escolhaComputador)
-    if (escolhaComputador === "pedra")
-        alert("Empatou")
-    else if (escolhaComputador === "papel")
-        alert("Perdeu")
-    else if (escolhaComputador === "tesoura")
-        alert("ganhou")
+    if (escolhaComputador === "pedra") {
+        pedraPc.style.border = "1px solid #ff0000";
+        papelPc.style.border = "none";
+        tesouraPc.style.border = "none";
+        empate.style.display = "block";
+    }
+    else if (escolhaComputador === "papel") {
+        papelPc.style.border = "1px solid #ff0000";
+        pedraPc.style.border = "none";
+        tesouraPc.style.border = "none";
+        pontuacaoComputador++;
+        empate.style.display = "none";
+    }
+    else if (escolhaComputador === "tesoura") {
+        tesouraPc.style.border = "1px solid #ff0000"
+        pedraPc.style.border = "none";
+        papelPc.style.border = "none";
+        pontuacaoJogador++;
+        empate.style.display = "none";
+    }
+    iptPontucaoComputador.value = pontuacaoComputador;
+    iptPontucaoJogador.value = pontuacaoJogador;
 });
 papel.addEventListener('click', () => {
     escolhaJogador = "papel";
     let escolhaComputador = getEscolhaComputador();
-    pc.innerHTML = `<h2>A escolha do computador foi:</h2>
-    <img src='img/${escolhaComputador}.png'/>`;
-    if (escolhaComputador === "pedra")
-        alert("Ganhou")
-    else if (escolhaComputador === "papel")
-        alert("Empatou")
-    else if (escolhaComputador === "tesoura")
-        alert("Perdeu")
+    if (escolhaComputador === "pedra") {
+        pedraPc.style.border = "1px solid #ff0000";
+        papelPc.style.border = "none";
+        tesouraPc.style.border = "none";
+        empate.style.display = "none";
+        pontuacaoJogador++;
+    }
+    else if (escolhaComputador === "papel") {
+        papelPc.style.border = "1px solid #ff0000";
+        pedraPc.style.border = "none";
+        tesouraPc.style.border = "none";
+        empate.style.display = "block";
+    }
+    else if (escolhaComputador === "tesoura") {
+        tesouraPc.style.border = "1px solid #ff0000"
+        pedraPc.style.border = "none";
+        papelPc.style.border = "none";
+        pontuacaoComputador++;
+        empate.style.display = "none";
+    }
+    iptPontucaoComputador.value = pontuacaoComputador;
+    iptPontucaoJogador.value = pontuacaoJogador;
 });
 tesoura.addEventListener('click', () => {
     escolhaJogador = "tesoura";
     let escolhaComputador = getEscolhaComputador();
-    console.log(escolhaComputador);
-    pc.innerHTML = `<h2>A escolha do computador foi:</h2>
-    <img src='img/${escolhaComputador}.png'/>`;
-    if (escolhaComputador === "pedra")
-        alert("Perdeu")
-    else if (escolhaComputador === "papel")
-        alert("Ganhou")
-    else if (escolhaComputador === "tesoura")
-        alert("Empatou")
+    if (escolhaComputador === "pedra") {
+        pedraPc.style.border = "1px solid #ff0000";
+        papelPc.style.border = "none";
+        tesouraPc.style.border = "none";
+        empate.style.display = "none";
+        pontuacaoComputador++;
+    }
+    else if (escolhaComputador === "papel") {
+        papelPc.style.border = "1px solid #ff0000";
+        pedraPc.style.border = "none";
+        tesouraPc.style.border = "none";
+        empate.style.display = "none";
+        pontuacaoJogador++;
+    }
+    else if (escolhaComputador === "tesoura") {
+        tesouraPc.style.border = "1px solid #ff0000"
+        pedraPc.style.border = "none";
+        papelPc.style.border = "none";
+        empate.style.display = "block";
+    }
+    iptPontucaoComputador.value = pontuacaoComputador;
+    iptPontucaoJogador.value = pontuacaoJogador;
 });
 
