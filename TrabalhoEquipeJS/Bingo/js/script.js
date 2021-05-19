8173e25ae6937a1cb7013f1a79b1a5e0d2c4b336
Maurício Fonseca <mauricio.fonseca@agiltec.com>
2021-05-18 22:47:48 -0300

    Código para sortear a pedra

--- TrabalhoEquipeJS/Bingo/js/script.js
+++ TrabalhoEquipeJS/Bingo/js/script.js
@@ -1,9 +1,11 @@
+let cartela = [];
+let pedras = [];
+let colunasTabela = document.getElementsByTagName("td");
+for (let i = 1; i <= 60; i++) {
+    pedras.push(i)
+}
 function gerarCartela() {
     event.preventDefault();
-    let cartela = [];
-    let pedras = [];
-    let colunasTabela = document.getElementsByTagName("td");
-
     const randomUnique = (range, count) => {
         let nums = new Set();
         while (nums.size < count) {
@@ -12,16 +14,22 @@ function gerarCartela() {
         return [...nums];
     }
 
-    for (let i = 1; i <= 60; i++) {
-        pedras.push(i)
-    }
+
 
     cartela = randomUnique(pedras.length, colunasTabela.length);
-    console.log(cartela);
-
     for (let i = 0; i < colunasTabela.length; i++) {
         colunasTabela[i].textContent = cartela[i];
     }
 
+}
+
+
+function sortearPedra() {
+    var pedraSelecionada = pedras.find((item) => item === Math.floor(Math.random() * pedras.length))
+    if (pedraSelecionada != undefined) {
+        alert(`A pedra selecionada é: ${pedraSelecionada}`);
+        let indexItemCartela = cartela.findIndex(item => item === pedraSelecionada)
+        colunasTabela[indexItemCartela].style.background = "red"
+    }
 }
 
