4e86ef22c1c267567272215f44972cdfe0e0e6c3
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2021-10-04 19:55:28 -0300

    Resolvendo exercício 1

---
+++ AtividadesPHP/Atividade-1/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    - Atribua um valor a uma variável e mostre se ele é um número positivo ou negativo.
+    - Atribua um valor a uma variável e mostre se ela é par ou ímpar; 
+*/
+echo 'Exercício 1';
+echo '======================================================';
+$numeroPositivoNegativo = 5;
+if ($numeroPositivoNegativo > 0) {
+    echo 'Número positivo';
+} else {
+    echo 'Número negatico';
+}
+echo 'Exercício 2';
+echo '======================================================';
+
+$numeroImparPar = 10;
+if ($numeroImparPar % 2 == 0) {
+    echo 'Número Par';
+} else {
+    echo 'Número Ímpar';
+}
